7b6a3ab9be01482d4e488e54d1df03ba9082d71d
Rich Bielby <richard.bielby@education.gov.uk>
2022-12-13 13:56:12 +0000

    Fixed bug in filter 1

--- ui.R
+++ ui.R
@@ -2,12 +2,14 @@ ui <- function(input, output, session) {
   mainPanel(
  fluidRow(
    width=12,
-   column(6,
-          selectizeInput("filter1",label='Filter 1',choices=c('England','North East','Yorkshire and the Humber'),
-                         selected='All')
+   column(4,
+          selectizeInput("filter1",label='Filter 1',
+                         choices=c('England','North East','Yorkshire and the Humber'),
+                         selected='England')
           ),
-   column(6,
-          selectizeInput("filter2",label='Filter 2',choices=c('All','Primary','Secondary'),
+   column(4,
+          selectizeInput("filter2",label='Filter 2',
+                         choices=c('All','Primary','Secondary'),
                          selected='All')
    )
  ) ,
