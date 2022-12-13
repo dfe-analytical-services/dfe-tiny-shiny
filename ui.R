c5af83b09ba1208e014dc5869124cfa1957ed1e9
Rich Bielby <richard.bielby@education.gov.uk>
2022-12-13 14:05:00 +0000

    Updated with tidy code and added fluidPage env

--- ui.R
+++ ui.R
@@ -1,23 +1,29 @@
 ui <- function(input, output, session) {
-  mainPanel(
- fluidRow(
-   width=12,
-   column(4,
-          selectizeInput("filter1",label='Filter 1',
-                         choices=c('England','North East','Yorkshire and the Humber'),
-                         selected='England')
-          ),
-   column(4,
-          selectizeInput("filter2",label='Filter 2',
-                         choices=c('All','Primary','Secondary'),
-                         selected='All')
-   )
- ) ,
-  fluidRow(
-    column(
-    12,
-    plotOutput("chart")
-    )
-    )
+  fluidPage(
+      fluidRow(
+        width = 12,
+        column(
+          4,
+          selectizeInput("filter1",
+            label = "Filter 1",
+            choices = c("England", "North East", "Yorkshire and the Humber"),
+            selected = "England"
+          )
+        ),
+        column(
+          4,
+          selectizeInput("filter2",
+            label = "Filter 2",
+            choices = c("All", "Primary", "Secondary"),
+            selected = "All"
+          )
+        )
+      ),
+      fluidRow(
+        column(
+          12,
+          plotOutput("chart")
+        )
+      )
   )
 }
